c7c972d61dd2b08e4b49e8bb9404a78256c3ec40
LEEJS-32 <jianshenglee32@gmail.com>
2025-02-21 17:25:26 +0800

    Update Assignment.java

--- src/assignment/Assignment.java
+++ src/assignment/Assignment.java
@@ -15,6 +15,7 @@ public class Assignment {
      */
     public static void main(String[] args) {
         System.out.println("helloworld");
+        System.out.println("Hello world again");
     }
     
 }
